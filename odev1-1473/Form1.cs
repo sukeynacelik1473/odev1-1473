bf72f35733fe6b05fbaa015d08edd98777aef9dc
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-17 14:01:06 +0300

    Add files via upload

---
+++ odev1-1473/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev1_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOdeme_Click(object sender, EventArgs e)
+        {
+            double tutar;
+            tutar = Convert.ToDouble(textbox1.Text);
+            if (radioButton2.Checked==true|| radioButton3.Checked == true)
+            {
+                tutar = tutar + (tutar * 0.05);
+            }
+            if(radioButton4.Checked==true|| radioButton5.Checked == true)
+            {
+                tutar = tutar + (tutar * 0.10);
+            }
+            MessageBox.Show("Ödencek toplam tutar:" + tutar.ToString() + "TL");
+        }
+    }
+}
